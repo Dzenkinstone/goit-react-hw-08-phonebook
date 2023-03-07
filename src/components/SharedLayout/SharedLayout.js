7a15a4fcacd75899ef97f14226ede7d017221357
Dzenkinstone <kozlovdanil6@gmail.com>
2023-03-07 23:11:32 +0100

    changes

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,9 +1,7 @@
 import { AppBar } from 'components/AppBar';
-import { Navigation } from 'components/Navigation';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container } from './SharedLayout.styled';
-import { Header } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
