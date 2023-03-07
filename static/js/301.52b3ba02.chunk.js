063dc9456e41903af9792ae4da36022bcb80ff22
Dzenkinstone <108086847+Dzenkinstone@users.noreply.github.com>
2023-03-07 22:25:36 +0000

    Deploying to gh-pages from @ Dzenkinstone/goit-react-hw-08-phonebook@7a15a4fcacd75899ef97f14226ede7d017221357 🚀

---
+++ static/js/301.52b3ba02.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[301],{6301:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r,c=t(168),i=t(7402).Z.div(r||(r=(0,c.Z)(["\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  margin-top: 50px;\n"]))),u=t(184),a=function(){return(0,u.jsx)(i,{children:(0,u.jsx)("h1",{children:"Welcome to the page!"})})},o=function(){return(0,u.jsx)(a,{})}}}]);
+//# sourceMappingURL=301.52b3ba02.chunk.js.map
